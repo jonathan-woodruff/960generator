f493ce13c2135e707c18c469dae0eb8d165d46e2
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2025-03-12 14:01:12 -0500

    finished board and finished first draft of js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const generateButton = document.getElementById('generate-button');
 
 const placePieceRandomly = (availableSlots, selectedPositions, pieceStr) => {
     let randomIndex = Math.floor(Math.random() * availableSlots.length);
-    selectedPositions.push(availableSlots[randomIndex]);
+    selectedPositions[availableSlots[randomIndex]] = pieceStr;
     availableSlots.splice(randomIndex, 1);
     return [availableSlots, selectedPositions];
 };
@@ -11,7 +11,7 @@ const placePieceRandomly = (availableSlots, selectedPositions, pieceStr) => {
 const placeBishop2 = (availableSlots, selectedPositions) => {
     //The other bishop must be placed on an different-colored square as the first bishop
     const bishop1Position = selectedPositions[0];
-    let bishop2AvailableSlots = [];
+    let bishop2AvailableSlots;
     if (bishop1Position === 1 || bishop1Position === 3 || bishop1Position === 5 || bishop1Position === 7) {
         bishop2AvailableSlots = [2, 4, 6, 8]
     } else {
@@ -19,31 +19,46 @@ const placeBishop2 = (availableSlots, selectedPositions) => {
     }
     let randomIndex = Math.floor(Math.random() * 4);
     const bishop2Position = bishop2AvailableSlots[randomIndex];
-    selectedPositions.push(bishop2Position);
+    selectedPositions[bishop2Position] = "B";
     availableSlots.splice(availableSlots.findIndex(bishop2Position), 1);
     return [availableSlots, selectedPositions];
 };
 
 const placeRooksAndKing = (availableSlots, selectedPositions) => {
     //place rook 1, then the king, then rook 2
-    for (let i = 0; i < 3; i++) {
-        selectedPositions.push(availableSlots.shift());
-    }
+    selectedPositions[availableSlots.shift()] = "R"; //rook 1
+    selectedPositions[availableSlots.shift()] = "K"; //king
+    selectedPositions[availableSlots.shift()] = "R"; //rook 2
     return selectedPositions;
     
 };
 
 const doSetup = () => {
-    let availableSlots = [1, 2, 3, 4, 5, 6, 7, 8];
-    let selectedPositions = ["Z", "Z", "Z", "Z", "Z", "Z", "Z", "Z"]; //Z is a placeholder
-    [availableSlots, selectedPositions] = placePieceRandomly(availableSlots, selectedPositions); //bishop 1
+    let availableSlots = [0, 1, 2, 3, 4, 5, 6, 7];
+    let selectedPositions = ["", "", "", "", "", "", "", ""];
+    [availableSlots, selectedPositions] = placePieceRandomly(availableSlots, selectedPositions, "B"); //bishop 1
     [availableSlots, selectedPositions] = placeBishop2(availableSlots, selectedPositions); //bishop 2
-    [availableSlots, selectedPositions] = placePieceRandomly(availableSlots, selectedPositions); //knight 1
-    [availableSlots, selectedPositions] = placePieceRandomly(availableSlots, selectedPositions); //knight 2
-    [availableSlots, selectedPositions] = placePieceRandomly(availableSlots, selectedPositions); //Queen
+    [availableSlots, selectedPositions] = placePieceRandomly(availableSlots, selectedPositions, "N"); //knight 1
+    [availableSlots, selectedPositions] = placePieceRandomly(availableSlots, selectedPositions, "N"); //knight 2
+    [availableSlots, selectedPositions] = placePieceRandomly(availableSlots, selectedPositions, "Q"); //Queen
     selectedPositions = placeRooksAndKing(availableSlots, selectedPositions); //rook 1, king, rook 2
-    // Now, selectedPositions stores the position corresponding to [B1, B2, N1, N2, Q, R1, K, R2]
-
+    //display the randomly generated starting position
+    document.getElementById('a1').innerHTML = selectedPositions[0];
+    document.getElementById('a8').innerHTML = selectedPositions[0];
+    document.getElementById('b1').innerHTML = selectedPositions[1];
+    document.getElementById('b8').innerHTML = selectedPositions[1];
+    document.getElementById('c1').innerHTML = selectedPositions[2];
+    document.getElementById('c8').innerHTML = selectedPositions[2];
+    document.getElementById('d1').innerHTML = selectedPositions[3];
+    document.getElementById('d8').innerHTML = selectedPositions[3];
+    document.getElementById('e1').innerHTML = selectedPositions[4];
+    document.getElementById('e8').innerHTML = selectedPositions[4];
+    document.getElementById('f1').innerHTML = selectedPositions[5];
+    document.getElementById('f8').innerHTML = selectedPositions[5];
+    document.getElementById('g1').innerHTML = selectedPositions[6];
+    document.getElementById('g8').innerHTML = selectedPositions[6];
+    document.getElementById('h1').innerHTML = selectedPositions[7];
+    document.getElementById('h8').innerHTML = selectedPositions[7];
 };
 
 generateButton.addEventListener('click', doSetup);
